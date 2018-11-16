973fbd28424a76c25ca4584e3e638c035e0d793b
ewerton.lucena <ewerton.lucena@BRDTITEC01.ilumno.net>
2018-11-16 15:02:01 -0200

    Ewerton

---
+++ TrabalhoA2/src/dados/preRequisitos.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dados;
+
+/**
+ *
+ * @author 20141104662
+ */
+public interface preRequisitos {
+   int TEMPOMAXUSO = 30;
+   int IDADEMINIMAMOTORISTA = 18;
+   
+}
