9ed79ba80ccba30085d7fe4733263907a74fb7e0
20141104662 <20141104662@TJINLA09E17.LABUVA.EDU>
2018-11-19 20:39:14 -0200

    Ewerton

--- TrabalhoA2/src/dados/Moto.java
+++ TrabalhoA2/src/dados/Moto.java
@@ -5,10 +5,16 @@ public class Moto extends Veiculo {
     private int volumeBagageiro;
     
     public void setVolumeBagageiro(int volumeBagageiro){
+        if(volumeBagageiro < 0){
+            throw new IllegalArgumentException ("Não são permitidos valores negativos");
+        }
         this.volumeBagageiro = volumeBagageiro;
     }
         public int getVolumeBagageiro(){
             return this.volumeBagageiro;
     }
-        
+        public Moto(){
+            super.nomeVeiculo = MOTO;
+            super.rpmVeiculo = MOTORPM;
+        }
 }
