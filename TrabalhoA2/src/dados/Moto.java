e3ab02083b9e79145cb036fe91abb4b170a89793
ewertonglucena <44753431+ewertonglucena@users.noreply.github.com>
2018-11-20 22:38:36 -0200

    Ewerton

--- TrabalhoA2/src/dados/Moto.java
+++ TrabalhoA2/src/dados/Moto.java
@@ -10,11 +10,76 @@ public class Moto extends Veiculo {
         }
         this.volumeBagageiro = volumeBagageiro;
     }
-        public int getVolumeBagageiro(){
-            return this.volumeBagageiro;
+    
+    public int getVolumeBagageiro(){
+        return this.volumeBagageiro;
     }
-        public Moto(){
-            super.nomeVeiculo = MOTO;
-            super.rpmVeiculo = MOTORPM;
-        }
+    
+    public Moto(){
+        super.nomeVeiculo = MOTO;
+        super.rpmVeiculo = MOTORPM;
+    }
+    
+    public Moto(int volumeBagageiro){
+        this.setVolumeBagageiro(volumeBagageiro);
+        super.nomeVeiculo = MOTO;
+        super.rpmVeiculo = MOTORPM;
+    }
+    
+    public Moto(String proprietario, String marca, String placa,int numeroPassageiros, double preco, Motor motor, int volumeBagageiro){
+        super(proprietario, marca, placa, numeroPassageiros, preco, motor);
+        this.setVolumeBagageiro(volumeBagageiro);
+        super.nomeVeiculo = MOTO;
+        super.rpmVeiculo = MOTORPM;
+    }
+    
+    public Moto(String proprietario, String marca, String placa,int numeroPassageiros, double preco, Motor motor){
+        super(proprietario, marca, placa, numeroPassageiros, preco, motor);
+        super.nomeVeiculo = MOTO;
+        super.rpmVeiculo = MOTORPM;
+    }
+    
+    public Moto(String proprietario, String marca, String placa, int volumeBagageiro){
+        super(proprietario, marca, placa);
+        this.setVolumeBagageiro(volumeBagageiro);
+        super.nomeVeiculo = MOTO;
+        super.rpmVeiculo = MOTORPM;
+    }
+    
+    public Moto(int numeroPassageiros , Motor motor, int volumeBagageiro){
+        super(numeroPassageiros, motor);
+        this.setVolumeBagageiro(volumeBagageiro);
+        super.nomeVeiculo = MOTO;
+        super.rpmVeiculo = MOTORPM;
+    }
+    
+    public Moto(int numeroPasageiros, double preco, int volumeBagageiro){
+        super(numeroPasageiros, preco);
+        this.setVolumeBagageiro(volumeBagageiro);
+        super.nomeVeiculo = MOTO;
+        super.rpmVeiculo = MOTORPM;
+    }
+    
+    public Moto(String marca, int numeroPassageiros , double preco, int volumeBagageiro){
+        super(marca, numeroPassageiros, preco);
+        this.setVolumeBagageiro(volumeBagageiro);
+        super.nomeVeiculo = MOTO;
+        super.rpmVeiculo = MOTORPM;
+    }
+    
+    public Moto(double preco, String proprietario, int numeroPassageiros, int volumeBagageiro){
+        super(preco, proprietario, numeroPassageiros);
+        this.setVolumeBagageiro(volumeBagageiro);
+        super.nomeVeiculo = MOTO;
+        super.rpmVeiculo = MOTORPM;
+    }
+    
+    public Moto(int numeroPassageiros, String marca ,Motor motor, String placa ,double preco, String proprietario, int volumeBagageiro){
+        super(numeroPassageiros, marca , motor, placa , preco, proprietario);
+        this.setVolumeBagageiro(volumeBagageiro);
+        super.nomeVeiculo = MOTO;
+        super.rpmVeiculo = MOTORPM;
+    }
+    
+    
 }
