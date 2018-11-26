27c39fc9d77561f82a2367b78a61cc100ea7ab2b
ewertonglucena <44753431+ewertonglucena@users.noreply.github.com>
2018-11-25 22:50:55 -0200

    Merge origin/master
    Conflicts:
    	TrabalhoA2/src/trabalhoa2/TrabalhoA2.java

--- TrabalhoA2/src/trabalhoa2/TrabalhoA2.java
+++ TrabalhoA2/src/trabalhoa2/TrabalhoA2.java
@@ -6,25 +6,48 @@ import dados.*;
  */
 public class TrabalhoA2 {
     public static void main(String[] args) {
+    try{
         int j =10;
         Carro car[] = new Carro[j];
-        Moto m1[] = new Moto[j];
-        Onibus o1[] = new Onibus[j];
+        Moto mot[] = new Moto[j];
+        Onibus oni[] = new Onibus[j];
+        for(int i = 0; i<j; i++){
+            car[i] = new Carro();
+            car[i].entradaDados();
+            car[i].imprimir();
+            car[i].acelerar();
+            car[i].rpmAtual();
+            car[i].desacelerar();
+            car[i].rpmAtual();
+            car[i].frear();
+            car[i].virarDireita();
+            car[i].virarEsquerda();
         
-    for(int i = 0; i<j; i++){
-        car[i] = new Carro();
-        car[i].entradaDados();
-        car[i].imprimir();
-        car[i].acelerar();
-        car[i].rpmAtual();
-        car[i].desacelerar();
-        car[i].rpmAtual();
-        car[i].frear();
-        car[i].virarDireita();
-        car[i].virarEsquerda();
+            mot[i] = new Moto();
+            mot[i].entradaDados();
+            mot[i].imprimir();
+            mot[i].acelerar();
+            mot[i].rpmAtual();
+            mot[i].desacelerar();
+            mot[i].rpmAtual();
+            mot[i].frear();
+            mot[i].virarDireita();
+            mot[i].virarEsquerda();
         
-        m1[i].entradaDados();
-        o1[i].entradaDados();
+            oni[i] = new Onibus();
+            oni[i].entradaDados();
+            oni[i].imprimir();
+            oni[i].acelerar();
+            oni[i].rpmAtual();
+            oni[i].desacelerar();
+            oni[i].rpmAtual();
+            oni[i].frear();
+            oni[i].virarDireita();
+            oni[i].virarEsquerda();
+            }
+        }
+        catch(NullPointerException | IndexOutOfBoundsException e){
+            System.out.println("Erro: "+e.getMessage());
+        }
     }
-    }    
 }
